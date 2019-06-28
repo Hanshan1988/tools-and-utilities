607a1552e21e8ce0363ca0354d108aa0661effd7
Work <work@Hanss-MacBook-Pro.local>
2019-06-28 13:20:13 +1000

    added pandas func

---
+++ pandas/df_util.py
@@ -0,0 +1,11 @@
+def get_sampled_sets(df_list, strat_cols=[], frac=0.1):
+    sample_func = lambda x: x.sample(frac=frac)
+    if len(strat_cols) > 0:
+        df_list_out = (df.groupby(strat_cols, group_keys=False).apply(sample_func) for df in df_list)
+    else:
+        df_list_out = (df.sample(frac=frac) for df in df_list)
+    return df_list_out
+
+def get_column_types_dict(df):
+	g = df.columns.to_series().groupby(df.dtypes).groups
+	return {k.name: v for k, v in g.items()}
