6296fa0daffefe7b667c2ec5eb457b90522273fe
Hans Han <hanshan@Hanss-MacBook-Pro.local>
2019-06-30 19:54:03 +1000

    fixed plotly

--- plotly/plotly_util.py
+++ plotly/plotly_util.py
@@ -55,8 +55,8 @@ def plot_2d_bar(df, x, y, xlab ='', ylab='', title='', pct=False, filename='2d-b
         ) for y_uniq_n in y_uniq]
     
     layout = go.Layout(
-        barmode='stack',
-        'xaxis':{'title':x})
+        barmode='stack'
+        )
     
     fig = go.Figure(data=data, layout=layout)
     
