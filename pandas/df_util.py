b37b8aeeb517cd3b493f1a282217fe4eff2f59a2
Work <work@Hanss-MacBook-Pro.local>
2019-08-01 10:01:33 +1000

    added to matplotlib and pandas

--- pandas/df_util.py
+++ pandas/df_util.py
@@ -6,13 +6,24 @@ def get_sampled_sets(df_list, strat_cols=[], frac=0.1):
         df_list_out = (df.sample(frac=frac) for df in df_list)
     return df_list_out
 
+
 def get_column_types_dict(df):
 	g = df.columns.to_series().groupby(df.dtypes).groups
 	return {k.name: v for k, v in g.items()}
 
-def upweigh_df(df, col, value, weight):
-    """Upweight the dataframe where col == value
-    """
-    df_extra = df[df[key] == value]
-    df_reweighed = df.append([df_extra] * (weight - 1), ignore_index=True)
-    return df_reweighed
+
+def reweigh_by_criteria(data, filter_key, filter_value, weight_name):        
+    extra = data[data[filter_key] == filter_value]
+    n_times = extra[weight_name].unique()
+    if len(n_times) > 1:
+        raise ValueError('This function assumes a unique weight in the filtered data.')
+        return 
+    reweighed = data.append([extra] * (n_times[0] - 1), ignore_index=True)
+    reweighed = reweighed.drop([weight_name], axis=1).reset_index(drop=True)
+    return reweighed
+
+
+def reweigh_by_row_weight(data, weight_name):
+    expanded = data.loc[data.index.repeat(data[weight_name])].assign()
+    reweighed = expanded.drop([weight_name], axis=1).reset_index(drop=True)
+    return reweighed
